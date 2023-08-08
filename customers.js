f9402c625e332ba749380b4cf6bbc960b13d063d
VAGHELA ALPESH RAKESHKUMAR <87261314+Y0ooSibbi@users.noreply.github.com>
2023-08-08 22:42:19 +0530

    Create customers.js

---
+++ customers.js
@@ -0,0 +1,6 @@
+const customers = [
+  { id: 1, first_name: 'Aman', last_name: 'Gupta', city: 'Ahmedabad', company: 'SublimeDataSystems' },
+  // Add more customers here
+];
+
+module.exports = customers;
